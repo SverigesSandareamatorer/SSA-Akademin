0d4c74eda272f6534c62aec66630a55268c313b7
k7per <94111680+k7per@users.noreply.github.com>
2022-01-18 14:58:47 +0100

    Simplified figure in BW
    Simplified the figure along the lines that Lorentz suggested and also rewrote the text to go along with the new figure.
    
    Please check that it is not too simplistic now.

--- macros/bild_tx_heat.m
+++ macros/bild_tx_heat.m
@@ -5,15 +5,18 @@ x = 0:0.03:1;
 v = x*360;
 uut = 5*sin(x*2*pi);
 ut = 6-uut;
-it = 2.5*sin(x*2*pi);
-pt = abs(ut.*it);
-plot(v, uut, "-k;U_{ut};", v, ut, ".k;U_t;", v, it, "+k;I_t;", v, pt, "--k;P_t;");
-xlabel("Vinkel (grader)");
-ylabel("Spänning, ström, effekt");
+il = ut/10;
+it = 1.2-il;
+pt = ut .* it;
+
+plot(v, ut, "-k;U_{ce};", v, pt, ".k;P_t;");
+
+
+xlabel("Angle (degrees)");
+ylabel ("Voltage (V) and Power (W)");
 
 file_path = fileparts(mfilename('fullpath'))
 
 file_name = [file_path  "/" "bild_tx_heat.eps"]
 
-# print -dpdf file_name
 print ("-deps", file_name);
